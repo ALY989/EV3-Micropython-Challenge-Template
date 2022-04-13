d42b419e94b660be23a9349d5709b0743a73edc5
rake6682 <andy.t.ly3@gmail.com>
2022-04-13 14:48:54 -0700

    mission 3 outllne

--- mission3.py
+++ mission3.py
@@ -18,7 +18,37 @@ def run():
 
     m.startup()
 
-    pass
+    30 degree angle
+
+    d.straight(1210)
+
+    d.turn(90)
+
+    d.straight(200)
+
+    d.turn(90)
+
+    d.run_until_stalled(-1000)
+
+    d.reset_angle(0)
+
+    d.run_target(1000, 35)
+
+    d.straight(260)
+
+    d.straight(-260)
+
+    d.turn(90)
+
+    d.straight(50)
+
+    d.turn(90)
+
+    d.straight(260)
+
+    d.straight(-260)
+
+    d.turn(90)
 
     # Put what the robot should do for this mission here. 
 
